a2baf050f6e252f8134b74f85079217a131c1a35
shakiernan26 <74791604+shakiernan26@users.noreply.github.com>
2020-11-20 14:15:16 -0500

    Add files via upload

---
+++ Project5/src/prj5/Input.java
@@ -0,0 +1,25 @@
+package prj5;
+
+//Virginia Tech Honor Code Pledge:
+//
+//As a Hokie, I will conduct myself with honor and integrity at all times.
+//I will not lie, cheat, or steal, nor will I accept the actions of those who
+//do.
+//-- Shane Kiernan (kshane6)
+/**
+* @author Shane Kiernan (kshane6)
+* @version 11.20.2020
+*
+*
+*          Input Class for Porject 5
+*/
+
+public class Input {
+    
+    
+    
+    public void main(String[] args) {
+        
+    }
+
+}
